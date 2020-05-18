29eb162cc0b6c2f949eb75ca0898d9db2eefd61e
sa9rbm <yishan_yang@saic-gm.com>
2020-05-18 17:25:52 +0800

    完成作业

--- src/main/java/com/jkxy/car/api/controller/CarController.java
+++ src/main/java/com/jkxy/car/api/controller/CarController.java
@@ -6,6 +6,7 @@ import com.jkxy.car.api.utils.JSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,6 +16,39 @@ import java.util.List;
 public class CarController {
     @Autowired
     private CarService carService;
+
+    /**
+     * 卖车接口
+     * @param carSeries
+     * @return
+     */
+    @GetMapping("buyCar/{carSeries}")
+    public JSONResult buyCar(@PathVariable String carSeries){
+        List<Car> cars = carService.findBySeries(carSeries);
+        if(cars == null ||cars.size() == 0){
+            return JSONResult.errorMsg( carSeries + " has already sold out.");
+        }
+        Car c = cars.get(0);
+        return this.deleteById(c.getId());
+    }
+
+    /**
+     * 模糊查询某品牌
+     * 返回最后5条数据
+     * @param keyword
+     * @return
+     */
+    @GetMapping("search/{keyword}")
+    public JSONResult search(@PathVariable String keyword){
+        List<Car> cars = carService.search(keyword);
+        List<Car> res =new ArrayList<>();
+        //
+        for (int i = cars.size() - 1; i >= 0 && i >= cars.size() - 5 ; i--) {
+            res.add(cars.get(i));
+        }
+        return JSONResult.ok(res);
+    }
+
 
     /**
      * 查询所有
@@ -85,4 +119,6 @@ public class CarController {
         carService.insertCar(car);
         return JSONResult.ok();
     }
+
+
 }
